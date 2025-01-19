14742cbce7bd404ac012a07e4a6b481ca3b09583
Asma <asma.rihani@cruxsoft.be>
2025-01-19 18:31:05 +0100

    refactor: extract isSulfurasItem and it's process

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,29 +22,35 @@ class GildedRose {
     private void updateItem(Item item) {
         boolean isAgedBrieItem = AGED_BRIE.equals(item.name);
         boolean isBackStagePass = BACKSTAGE_PASSES.equals(item.name);
+        boolean isSulfurasItem = SULFURAS.equals(item.name);
         if (isAgedBrieItem) {
             updateAgedBrie(item);
         } else {
             if (isBackStagePass) {
                 updateBackStagePassItem(item);
             } else {
-                if (item.quality > 0) {
-                    if (!SULFURAS.equals(item.name)) {
+                if (isSulfurasItem) {
+                    if (item.quality > 0) {
+                    }
+
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                        }
+                    }
+                } else {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
 
-                if (!SULFURAS.equals(item.name)) {
                     item.sellIn = item.sellIn - 1;
-                }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!SULFURAS.equals(item.name)) {
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
                     }
                 }
+
             }
 
         }
