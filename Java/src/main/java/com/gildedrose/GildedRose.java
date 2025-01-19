6a282d2ee2f88e8fb730329aac4c2381ff90a5a9
Asma <asma.rihani@cruxsoft.be>
2025-01-19 18:34:46 +0100

    refactor: simplify updateOrdinaryItem method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -34,17 +34,12 @@ class GildedRose {
     }
 
     private void updateOrdinaryItem(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
+        if (item.sellIn > 0) {
+            item.quality = Math.max(item.quality - 1, MIN_QUALITY);
+        } else {
+            item.quality = Math.max(item.quality - 2, MIN_QUALITY);
         }
-
         item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
     }
 
     private void updateBackStagePassItem(Item item) {
