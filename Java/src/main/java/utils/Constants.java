00ff554a47db546d8cce4a058149149520e0307d
Asma <asma.rihani@cruxsoft.be>
2025-01-19 16:47:32 +0100

    refactor: use constants for items names

---
+++ Java/src/main/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public final class Constants {
+    public static final String ORDINARY_ITEM = "foo";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+
+    private Constants() {}
+}
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,16 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.Constants.ORDINARY_ITEM;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item(ORDINARY_ITEM, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(ORDINARY_ITEM, app.items[0].name);
     }
 
 }
