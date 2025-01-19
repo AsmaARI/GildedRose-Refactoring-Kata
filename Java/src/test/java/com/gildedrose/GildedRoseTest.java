04f811ce0f87a5fa18cf3abfea1dd4ce90b69fb7
Asma <asma.rihani@cruxsoft.be>
2025-01-19 17:18:52 +0100

    test: add tests for Aged Brie item

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.Constants.AGED_BRIE;
 import static utils.Constants.ORDINARY_ITEM;
 
 class GildedRoseTest {
@@ -61,4 +62,48 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].quality);
     }
 
+    /**
+     * Aged Brie item
+     * Expiration has not passed
+     * quality increase by 1
+     * sellIn decrease by 1
+     */
+    @Test
+    void qualityAndSellInForAgedBrieItem() {
+        Item[] items = new Item[] { new Item(AGED_BRIE, 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+    }
+
+    /**
+     * Aged Brie item
+     * Expiration has passed
+     * quality increase by 2
+     * sellIn decrease by 1
+     */
+    @Test
+    void qualityAndSellInForAgedBrieItemWithPassedExpiration() {
+        Item[] items = new Item[] { new Item(AGED_BRIE, -2, 4) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-3, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+    }
+
+    /**
+     * Aged Brie item
+     * Expiration has not passed
+     * quality is max
+     * sellIn decrease by 1
+     */
+    @Test
+    void qualityAndSellInForAgedBrieItemWithMaxQuality() {
+        Item[] items = new Item[] { new Item(AGED_BRIE, 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
 }
