e4eb397605afe2475faccd2bc2f939849945d3d3
Asma <asma.rihani@cruxsoft.be>
2025-01-19 17:04:37 +0100

    test: add tests for ordinary item

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,4 +15,50 @@ class GildedRoseTest {
         assertEquals(ORDINARY_ITEM, app.items[0].name);
     }
 
+    /**
+     * Ordinary item
+     * Expiration has not passed
+     * quality decrease by 1
+     * sellIn decrease by 1
+     */
+    @Test
+    void qualityAndSellInForOrdinaryItem() {
+        Item[] items = new Item[] { new Item(ORDINARY_ITEM, 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    /**
+     * Ordinary item
+     * Expiration has passed
+     * quality decrease by 2
+     * sellIn decrease by 1
+     */
+    @Test
+    void qualityAndSellInForOrdinaryItemWithPassedExpiration() {
+        Item[] items = new Item[] { new Item(ORDINARY_ITEM, 0, 4) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
+
+    /**
+     * Ordinary item
+     * Expiration has passed
+     * quality decrease by 2
+     * sellIn decrease by 1
+     * quality can not be less than 0
+     */
+    @Test
+    void qualityAndSellInForOrdinaryItemNeverNegative() {
+        Item[] items = new Item[] { new Item(ORDINARY_ITEM, 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
 }
