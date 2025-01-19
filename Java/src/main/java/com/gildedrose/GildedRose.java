579035b4ab6ea38c8eeff92edd27c87860b4ad72
Asma <asma.rihani@cruxsoft.be>
2025-01-19 18:24:09 +0100

    refactor: clean up the code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -41,9 +41,7 @@ class GildedRose {
                     }
                 }
 
-                if (!SULFURAS.equals(item.name)) {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
                     item.quality = 0;
